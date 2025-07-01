7b2a848d11b79dd8b0552fc12195c13424e61cba
tonyha <ihasdag@gmail.com>
2025-07-01 17:06:52 +0900

    [Setup] Spring Boot Project Settings

---
+++ backend/src/main/java/com/stocktrum/backend/controllers/ExampleController.java
@@ -0,0 +1,13 @@
+package com.stocktrum.backend.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExampleController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello World!";
+    }
+}
